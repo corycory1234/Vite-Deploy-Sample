41059ca3dc8753d2214a5e7da9d2ae2812479c81
corycory1234 <note10litenote10lite@gmail.com>
2023-05-06 09:36:50 +0800

    Vite專案測試第1版

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <p class="data1">{{ data1 }}</p>
+      <p>{{ data2 }}</p> 
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView, } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue"
+// (1)import axios進來
+import axios from 'axios'
+
+const data1 = ref("")
+const data2 = ref("")
+
+// (1)axios語法，取得response.data
+axios.get("https://randomuser.me/api/")
+  .then((response)=> {
+    console.log(response);
+    data1.value =  response.data.results[0].location.city;
+    data2.value = response.data.results[0].email
+  })
+
+</script>
+
+<style scoped>
+.data1{
+  color: sandybrown;
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+  export default{
+    data() {
+      return {
+        data: {},
+      }
+    },
+    mounted(){
+      // 開發中環境變數 ↓
+      console.log(import.meta.env.VITE_TEXT);
+      // 通用環境變數 ↓
+      // url變數 賦值import....
+      const url = import.meta.env.VITE_PATH;
+      // 再把radomuser網址換成 url變數
+      this.$http.get(url)
+        .then((response) => {console.log(response);
+        this.data = response.data.results[0];
+        console.log(this.data);
+      });
+
+
+    }
+  }
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
